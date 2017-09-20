b92ad25234b357b3d3146e86d45ad88aeb5de00c
788007 <788007@users.noreply.github.com>
2017-09-20 13:59:37 -0700

    Add files via upload

--- Start/canvasDemo.js
+++ Start/canvasDemo.js
@@ -20,8 +20,23 @@ function init(){
   // get the context
   ctx = canvas.getContext('2d'); // This is the context
   //create array of balls
-  for (var i = 0; i < 30; i++){
-    balls[i] = new Ball();
+  for (var i = 0; i < 5; i++){
+    var radius = Math.random()*20 + 10;
+    var color = randomColor();
+    //set location vector
+    var x = Math.random() * (canvas.width-20) + 10;
+    var y = Math.random() * (canvas.height-20) + 10;
+    var loc = new vector2d(x, y);
+    //set velocity vector
+    var r = (Math.random()* 5 + 1);
+    var theta = Math.random() * 2 * Math.PI;
+    var vel = new vector2d(undefined, undefined, r, theta);
+    //set acceleration vector
+    //var r2 = Math.random() / 2.0;
+    //var t = Math.random() * 2 * Math.PI;
+    var acc = new vector2d(0, 0);
+
+    balls[i] = new Ball(radius, loc, vel, acc, color);
   }
 
   animate(); // Call to your animate function
@@ -37,43 +52,27 @@ function randomColor(){
 
 
 //creates ball objects when called w/ new
-function Ball(){
-  this.radius = Math.random()*20 + 10;
-  this.color = randomColor();
-
+function Ball(radius, loc, vel, acc, color){
+  this.radius = radius;
+  this.color = color;
   //let volume = area and density = 1, so mass = area
   this.mass = Math.PI * this.radius * this.radius;
-
-  //set location vector
-  var x = Math.random() * (canvas.width-20) + 10;
-  var y = Math.random() * (canvas.height-20) + 10;
-  this.loc = new vector2d(x, y);
-
-  //set velocity vector
-  var r = (Math.random()* 5 + 1);
-  var theta = Math.random() * 2 * Math.PI;
-  this.vel = new vector2d(undefined, undefined, r, theta);
-
-  //set acceleration vector
-  this.acc = new vector2d(0, 0);
-
-  this.momentum = this.vel.scalarMult(this.mass);
+  this.loc = loc;
+  this.vel = vel;
+  this.acc = acc;
+  //this.momentum = this.vel.scalarMult(this.mass);
 }
-
-
 
 //updates ball position
 Ball.prototype.update = function () {
   ball = this;
   ball.checkEdges();
   ball.loc.add(ball.vel);
-
-  //ball.vel.add(ball.acc);
+  ball.vel.add(ball.acc);
 }
 
 //reverses direction when ball hits edge
 Ball.prototype.checkEdges = function () {
-
   ball = this;
   if(ball.loc.x + ball.radius >= canvas.width){
     ball.loc.x = canvas.width - ball.radius;
@@ -93,36 +92,36 @@ Ball.prototype.checkEdges = function () {
   }
 }
 
-//check if balls should bounce off each other
-// function checkBallBounces () {
-//   for(var i = 0; i < balls.length -1; i++){
-//     for(var j = i + 1; j < balls.length; j++){
-//       //check if edges of 2 balls are touching
-//       var dist = balls[i].loc.distance(balls[j].loc);
-//       if( dist <= balls[i].radius + balls[j].radius){
-//
-//
-//       }
-//     }
-//   }
-//
-// }
-
 //draws ball
 Ball.prototype.draw = function () {
   ball = this;
+  ball.update();
   ctx.beginPath();
   ctx.arc(ball.loc.x, ball.loc.y, ball.radius, 0, Math.PI * 2);
   ctx.fillStyle = ball.color;
   ctx.fill();
 };
 
+function printMouseLoc(e){
+  var mouseX = e.clientX;
+  var mouseY = e.clientY;
+  console.log("x:", mouseX, "y:", mouseY);
+}
+
+function mouseAttract(){
+  console.log("clicked!");
+}
+
 function animate(){
   requestAnimationFrame(animate);
+
+  canvas.onclick = mouseAttract;
+
+  // canvas.onmousemove = function(event) {
+  //   printMouseLoc(event);
+  // }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for(var i = 0; i < balls.length; i++){
-    var ball = balls[i];
-    ball.update();
-    ball.draw();
+    balls[i].draw();
   }
 }
